833215ebe64f4ac46394f77ddcf21edd8c319a52
thomazvictorr <thomazvictorr@gmail.com>
2025-04-17 01:24:55 -0300

    Argumentos posicionais

---
+++ Fundamentos do Python/Módulo 1/argumentos-posicionais.py
@@ -0,0 +1,7 @@
+# Modifique a primeira linha de código no editor, usando as palavras-chave sep e end, para corresponder à saída esperada. Use as duas funções print() no editor.
+# Não altere nada na segunda invocação de print().
+# Saída esperada: 
+# Programação***Essenciais***em...Python
+
+print("Programação","Essenciais","em", sep="***", end="...")
+print("Python")
