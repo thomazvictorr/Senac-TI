cb2670bddcbe88ef329d75027601ed3ddb80baa8
thomazvictorr <thomazvictorr@gmail.com>
2025-04-18 15:03:38 -0300

    Operadores e Expressões

---
+++ Fundamentos do Python/Módulo 1/operadores-expressões.py
@@ -0,0 +1,19 @@
+# Dê uma olhada no código no editor: ele lê um valor float, coloca-o em uma variável chamada x e imprime o valor de uma variável chamada y. 
+# Sua tarefa é completar o código para avaliar a seguinte expressão:
+# 3x3 - 2x2 + 3x - 1
+# O resultado deve ser atribuído a y.
+
+x = 0
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print("y =", y)
+
+x = 1
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print("y =", y)
+
+x = -1
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print("y =", y)
