8b4c714fa60025be279fe863eac706077f734226
thomazvictorr <thomazvictorr@gmail.com>
2025-04-27 15:34:30 -0300

    Simples entradas e saídas

---
+++ Fundamentos do Python/Módulo 2/entradas-saídas.py
@@ -0,0 +1,12 @@
+# Sua tarefa é completar o código para avaliar os resultados de quatro operações aritméticas básicas.
+# Os resultados devem ser impressos no console.
+
+a = float(input("Entre com o primeiro valor: "))
+b = float(input("Entre com o segundo valor: "))
+
+print("Adição:", a + b)
+print("Subtração:", a - b)
+print("Multiplicação:", a * b)
+print("Divisão:", a / b)
+
+print("\nIsso é tudo, pessoal!")
