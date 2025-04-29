9e2111d5287dff21ba97977a5c32ba6ec4f7f131
thomazvictorr <thomazvictorr@gmail.com>
2025-04-29 10:52:57 -0300

    att repositório

--- Fundamentos do Python/Módulo 1/codificando-strings.py
+++
@@ -1,7 +0,0 @@
-# Agora, a próxima pergunta é: como você incorpora um apóstrofo em uma string colocada entre apóstrofos?
-# Você já deve saber a resposta ou, para ser mais preciso, duas respostas possíveis.
-# Tente imprimir uma string contendo a seguinte mensagem:
-# Eu sou Monty Python.
-
-print('I\'m Monty Python.')
-print("I'm Monty Python.")
