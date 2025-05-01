450f72d87abf8d0ae72e98d4a0fbefd61bba36ca
thomazvictorr <thomazvictorr@gmail.com>
2025-05-01 14:44:52 -0300

    Argumentos posicionais

---
+++ Fundamentos do Python/Módulo 2/codificando-strings.py
@@ -0,0 +1,7 @@
+# Agora, a próxima pergunta é: como você incorpora um apóstrofo em uma string colocada entre apóstrofos?
+# Você já deve saber a resposta ou, para ser mais preciso, duas respostas possíveis.
+# Tente imprimir uma string contendo a seguinte mensagem:
+# Eu sou Monty Python.
+
+print('I\'m Monty Python.')
+print("I'm Monty Python.")
