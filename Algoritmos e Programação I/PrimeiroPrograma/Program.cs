4cfbc78880d24560ab1034a97854a7e069f0bdb6
thomazvictorr <thomazvictorr@gmail.com>
2025-05-15 15:44:43 -0300

    Criando um Projeto C#

--- Algoritmos e Programação I/PrimeiroPrograma/Program.cs
+++ Algoritmos e Programação I/PrimeiroPrograma/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Olá, Mundo!");
+        Console.WriteLine("Algoritmos e Programação I");
     }
 }
