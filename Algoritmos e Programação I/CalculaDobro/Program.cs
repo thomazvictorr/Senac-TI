786010a82018b43e6f79a9e9595104532f3b1655
thomazvictorr <thomazvictorr@gmail.com>
2025-05-16 11:20:07 -0300

    Multiplicação do número por 2

---
+++ Algoritmos e Programação I/CalculaDobro/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CalculaDobro;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Informe um número: ");
+        int numero = int.Parse(Console.ReadLine());
+        int dobro = numero * 2;
+        Console.WriteLine("Dobro = " + dobro);
+    }
+}
