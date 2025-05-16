55a4006704d3b30c85e29e96cf08a3f2e0f70361
thomazvictorr <thomazvictorr@gmail.com>
2025-05-16 11:15:36 -0300

    Entrada e saída de dados

---
+++ Algoritmos e Programação I/SegundoPrograma/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SegundoPrograma;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Informe seu nome: ");
+        // comando de entrada de dados
+        string nome = Console.ReadLine();
+        // saida de dados
+        Console.WriteLine("Olá, " + nome + "!") ;
+    }
+}
