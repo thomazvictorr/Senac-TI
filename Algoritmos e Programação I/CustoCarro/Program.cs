a6be49e30a66aecf460f4c53861b8581ff6f1dc8
thomazvictorr <thomazvictorr@gmail.com>
2025-05-21 15:46:12 -0300

    Custo final do veiculo

---
+++ Algoritmos e Programação I/CustoCarro/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CustoCarro;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // entrada de dados
+        Console.Write("Informe o custo de fábrica: ");
+        double custoFabrica = double.Parse(Console.ReadLine());
+        //procesamento dos dados
+        double custoDistribuidor = custoFabrica * 0.28;
+        double imposto = custoFabrica * 0.45;
+        double custoConsumidor = custoFabrica + custoDistribuidor + imposto;
+        // saida de dados
+        Console.WriteLine("Custo ao consumidor: {0:N2}", custoConsumidor);
+    }
+}
