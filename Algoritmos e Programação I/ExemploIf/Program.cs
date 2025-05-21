15598d4833ff50e9015249739605fb565c89748d
thomazvictorr <thomazvictorr@gmail.com>
2025-05-21 16:35:43 -0300

    Exemplo de condição

---
+++ Algoritmos e Programação I/ExemploIf/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ExemploIf;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Velocidade: ");
+        int velocidade = int.Parse(Console.ReadLine());
+        if (velocidade > 60)
+        {
+            Console.WriteLine("Você excedeu o limite de velocidade");
+            Console.WriteLine("Multado");
+        }
+        Console.WriteLine("Fim do Programa!");
+    }
+
+}
