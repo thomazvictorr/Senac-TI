281884f7e47741fc1868bf2ff17f84e3a0b5d753
thomazvictorr <thomazvictorr@gmail.com>
2025-05-21 16:50:55 -0300

    Estruturas de condição

---
+++ Algoritmos e Programação I/ExemploIfElse/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ExemploIfElse;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Velocidade: ");
+        int velocidade = int.Parse(Console.ReadLine());
+        if (velocidade > 60)
+        {
+            Console.WriteLine("Multado!");
+        }
+        else
+        {
+            Console.WriteLine("Não Multado!");
+        }
+        Console.WriteLine("Fim do Programa.");
+    }
+}
