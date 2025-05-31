4087519db0bb5ff716d34818ca40f8b1e312bd7c
thomazvictorr <thomazvictorr@gmail.com>
2025-05-31 13:04:36 -0300

    Estrutura de repetição

---
+++ Algoritmos e Programação I/ExemploWhile/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ExemploWhile;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int cont = 1;
+        while (cont <= 10)
+        {
+            Console.WriteLine("Algoritmos");
+            cont = cont + 1;
+        }
+    }
+}
