d004cdcb3cde51909e68d7b1253fa5f352522159
thomazvictorr <thomazvictorr@gmail.com>
2025-05-31 13:10:42 -0300

    Estrutura de repetição definida

---
+++ Algoritmos e Programação I/ContagemRegressiva/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ContagemRegressiva;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int contador = 10;
+        while (contador > 0)
+        {
+            Console.WriteLine(contador);
+            contador = contador - 1;
+        }
+        Console.WriteLine("FOGO!!!1!");
+    }
+}
