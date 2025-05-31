1ac4e8aa459bc9824ea7f8389f266db567d852fa
thomazvictorr <thomazvictorr@gmail.com>
2025-05-31 12:31:26 -0300

    Mostra a classificação do IMC

---
+++ Algoritmos e Programação I/ExemploIMC/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ExemploIMC;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite seu Peso: ");
+        double peso = double.Parse(Console.ReadLine());
+        Console.Write("Digita sua Altura: ");
+        double altura = double.Parse(Console.ReadLine());
+
+        double imc = peso / (altura * altura);
+
+        Console.WriteLine("IMC: {0:N1}", imc);
+
+        if (imc < 20)
+        {
+            Console.WriteLine("Abaixo do peso.");
+        }
+        else if (imc < 25)
+        {
+            Console.WriteLine("Peso ideal.");
+        }
+        else
+        {
+            Console.WriteLine("Acima do peso.");
+        }
+    }
+}
