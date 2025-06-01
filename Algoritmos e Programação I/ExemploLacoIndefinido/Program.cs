e336d72d0a506a4e86c1053caeca5bc768a85291
thomazvictorr <thomazvictorr@gmail.com>
2025-06-01 15:07:50 -0300

    Estrutura de repetição com laço indefinido

---
+++ Algoritmos e Programação I/ExemploLacoIndefinido/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ExemploLacoIndefinido;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int soma = 0;
+        Console.Write("Informe um número: ");
+        int n = int.Parse(Console.ReadLine());
+        while (n >= 0)
+        {
+            soma = soma + n;
+            Console.Write("Informe um número: ");
+            n = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Soma:" + soma);
+    }
+}
