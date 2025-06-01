d6e859fbc29e4220f4887675212df2679ad2afb5
thomazvictorr <thomazvictorr@gmail.com>
2025-06-01 15:59:59 -0300

    Arquivo de configuração

---
+++ Algoritmos e Programação I/Tabuada/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Tabuada;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Informe um número: ");
+        int n = int.Parse(Console.ReadLine());
+        int r;
+        for (int cont = 1; cont <= 10; cont++)
+        {
+            r = n * cont;
+            Console.WriteLine("{0} x {1} = {2}", n, cont, r);
+        }
+    }
+}
