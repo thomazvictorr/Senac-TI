6ac4fe867e7ccc207effcb6b680d3918a18a43d7
thomazvictorr <thomazvictorr@gmail.com>
2025-06-01 16:34:57 -0300

    Calcula o Fatorial de um Número

---
+++ Algoritmos e Programação I/ExemploFuncoes/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ExemploFuncoes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        mensagem();
+        Console.WriteLine("Digite o número: ");
+        int numero = int.Parse(Console.ReadLine());
+        int fat = calculaFatorial(numero);
+        Console.WriteLine("Fatorial = " + fat);
+    }
+
+    static void mensagem()
+    {
+        Console.WriteLine("**** Calcula o Fatorial de um Número ****");
+    }
+
+    static int calculaFatorial(int n)
+    {
+        int fatorial = 1;
+        for (int cont = 1; cont <= n; cont++)
+        {
+            fatorial = fatorial * cont;
+        }
+        return fatorial;
+    }
+}
